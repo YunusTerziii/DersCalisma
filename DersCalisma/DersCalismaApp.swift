25e764f119ef4da90404ba7dc8469d56e1de75b6
yunusterziii <ynsterzi@gmail.com>
2026-01-31 02:44:05 +0300

    Initial Commit

---
+++ DersCalisma/DersCalismaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DersCalismaApp.swift
+//  DersCalisma
+//
+//  Created by Yunus Terzi on 31.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct DersCalismaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
